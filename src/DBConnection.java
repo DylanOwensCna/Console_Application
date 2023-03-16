1e8a576b6742c6a97d38f88ed07fb8527d36fd59
Dylan Owens <dylan.owens06@ed.cna.nl.ca>
2023-03-16 20:17:34 -0230

    Created and tested DBConnection. Also created SQLstatement for adding tables.

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,39 +1,71 @@
 import java.sql.*;
 public class DBConnection {
+    /**
+     * Enter credentials for db connection to HeidiSQL
+     */
+
+    protected static final String DB_URL = "jdbc:mariadb://localhost:3307/quote_application";
+    protected static final String DB_USER = "root";
+    protected static final String DB_PASS = "declan";
+    protected static final String MARIA_DB_DRIVER_NAME = "org.mariadb.jdbc.Driver";
+
     public static void main(String[] args) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        String url = "jdbc:mysql://localhost/mydb";
-        String user = "username";
-        String password = "password";
-        String sql = "INSERT INTO mytable (name, age) VALUES (?, ?)";
+
         try {
-            // Create a connection object
-            conn = DriverManager.getConnection(url, user, password);
+            /**
+             * register the driver
+             */
+                doClassForNameRegistration();
+                Connection connection = DriverManager.getConnection(
+                        DB_URL,
+                        DB_USER,
+                        DB_PASS
+                );
+            // Execute a query
+            Statement statement = connection.createStatement();
+            String autoTableCreate = "CREATE TABLE AutoPolicyQuotes " +
+                    "(quote_id INT NOT NULL AUTO_INCREMENT, " +
+//                    " policyHolderName VARCHAR(50) NOT NULL," +
+                    " policyCarValue DECIMAL(10,2) NOT NULL, " +
+                    " policyAccidentTotal INT NOT NULL, " +
+                    " policyDriverAge INT NOT NULL, " +
+                    " policyLocation INT NOT NULL, " +
+                    "quoteTotal DECIMAL(10,2) NOT NULL," +
+                    " PRIMARY KEY ( quote_id )" +
+                    ")";
 
-            // Prepare the SQL statement
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, "John");
-            pstmt.setInt(2, 25);
-
-            // Execute the SQL statement
-            int rows = pstmt.executeUpdate();
-            System.out.println(rows + " row(s) inserted.");
-
-        } catch (SQLException e) {
+            String homeTableCreate = "CREATE TABLE HomePolicyQuotes (" +
+                    "quoteId INT NOT NULL AUTO_INCREMENT," +
+//                    "policyHolderName VARCHAR(50) NOT NULL," +
+                    "policyHomeValue DECIMAL(10,2) NOT NULL," +
+                    "policyHomeAge INT NOT NULL," +
+                    "policyHeatingType INT NOT NULL," +
+                    "propertyValue DECIMAL(10,2) NOT NULL," +
+                    "quoteTotal DECIMAL(10,2) NOT NULL," +
+                    "PRIMARY KEY (quoteId)" +
+                    ");";
+            statement.executeUpdate(autoTableCreate);
+            statement.executeUpdate(homeTableCreate);
+            System.out.println("Table AutoPolicyQuotes created successfully!");
+            System.out.println("Table HomePolicyQuotes created successfully!");
+            connection.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            // Close the PreparedStatement and Connection objects
-            try {
-                if (pstmt != null) pstmt.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                if (conn != null) conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        }
+    }
+
+
+    public static void doClassForNameRegistration() {
+        final String driverName = MARIA_DB_DRIVER_NAME;
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Error: unable to load MariaDB driver class!");
+            System.out.printf("Driver Name: ", driverName);
+            System.exit(1);
         }
     }
 }
+
